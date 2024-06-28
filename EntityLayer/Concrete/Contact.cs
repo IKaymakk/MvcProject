dbc77bde46be305cabde6217948692e0fbff029c
İbrahim Kaymak <ibrahimkaymak817@outlook.com>
2024-06-28 20:51:21 +0300

    Web Config - Context

--- EntityLayer/Concrete/Contact.cs
+++ EntityLayer/Concrete/Contact.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.SqlServer.Server;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ namespace EntityLayer.Concrete
 {
     public class Contact
     {
+        [Key]
         public int ContactID { get; set; }
+        [StringLength(70)]
         public string Username { get; set; }
+        [StringLength(100)]
         public string UserMail { get; set; }
+        [StringLength(100)]
         public string Subject { get; set; }
         public string Message { get; set; }
     }
--- EntityLayer/Concrete/Heading.cs
+++ EntityLayer/Concrete/Heading.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -9,7 +10,9 @@ namespace EntityLayer.Concrete
 {
     public class Heading
     {
+        [Key]
         public int HeadingID { get; set; }
+        [StringLength(100)]
         public string HeadingName { get; set; }
         public DateTime HeadingDate { get; set; }
 
