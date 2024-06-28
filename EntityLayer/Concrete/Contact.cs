7232a75690067e17be2c03ab7291728e2406d8e6
ibrah <ibrah@KAYMAK>
2024-06-28 19:50:37 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public string Username { get; set; }
+        public string UserMail { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
