7effc9bb1b3c5bc4b79e6eef76cc556230173b10
İbrahim Kaymak <ibrahimkaymak817@outlook.com>
2024-06-29 13:47:09 +0300

    certificate commit

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    internal class CategoryManager
+    {
+        GenericRepository<Category> gr = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return gr.List();
+        }
+        public void CategoryAddBL(Category c)
+        {
+            //Şartları Katmanlı Mimariyi İçin Tanımlamadım
+            gr.Insert(c);
+        }
+    }
+}
