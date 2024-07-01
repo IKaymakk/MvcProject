f7114fadfd6c55e550eaf303318c4da4897fa1d6
İbrahim Kaymak <ibrahimkaymak817@outlook.com>
2024-07-01 23:31:47 +0300

    New Panel Theme, Writer List

---
+++ MvcProject/Controllers/WriterController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var count = wm.GetCount();
+            ViewBag.Count = count;
+            var values = wm.GetList();
+            return View(values);
+        }
+    }
+}
